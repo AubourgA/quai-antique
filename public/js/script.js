6f035441a993c223d48c3197dfed221f3f529a3a
AubourgA <adrien.aubourg@hotmail.fr>
2023-02-12 15:18:55 +0100

    create homepage and sliders

---
+++ public/js/script.js
@@ -0,0 +1,76 @@
+window.addEventListener('DOMContentLoaded', () => {
+  
+    
+    // MENU TOGGLE
+    
+    
+    // Gestion Carrousel
+    const carrousel = document.querySelector('.carrousel__wrapper');
+
+    const slides = Array.from(document.querySelectorAll('.carrousel__pics'));
+    
+    const previous = document.getElementById('arrow-left');
+    const next = document.getElementById('arrow-right');
+    
+    
+    let count = 0;
+    let carrouselWidth = carrousel.getBoundingClientRect().width;
+
+    //Duplicate first image into last slider
+    let firstImg = carrousel.firstElementChild.cloneNode(true);
+    carrousel.appendChild(firstImg);
+    
+
+    /**
+     * display next slide 
+    */
+   function nextSlide() {
+       count++;
+       carrousel.style.transition = "1s linear";
+       let rate = count * (-carrouselWidth);
+       carrousel.style.transform = `translateX(${rate}px)`;
+       
+       setTimeout( ()=> {
+            if(count >= slides.length) {
+                count = 0;
+                carrousel.style.transition = "unset";
+                carrousel.style.transform = "translateX(0)";
+            }
+        } , 1000);
+    }
+    
+     /**
+      * display previous slide
+      */
+    function prevSlide(){
+        count--;
+        carrousel.style.transition = "1s linear";
+
+        if(count < 0) {
+            count = slides.length;
+            let rate = count * (-carrouselWidth);
+            carrousel.style.transition = "unset";
+            carrousel.style.transform = `translateX(${rate}px)`;
+            setTimeout(prevSlide,1);
+        }
+
+        let rate = count * (-carrouselWidth);
+        carrousel.style.transform = `translateX(${rate}px)`;
+    };
+
+    //slider automatic
+    let sliderAutomatic = setInterval(nextSlide, 3000);
+
+    carrousel.addEventListener('mouseout', ()=> {
+        sliderAutomatic= setInterval(nextSlide,3000);
+    });
+    
+    carrousel.addEventListener('mouseover', ()=> {
+        clearInterval(sliderAutomatic);
+    });
+
+    //slider manual
+    next.addEventListener("click", nextSlide);
+    previous.addEventListener('click', prevSlide);
+})
+
