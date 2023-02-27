9ba745371b3c7a3c8ee99d46b52c354bee0332c3
AubourgA <adrien.aubourg@hotmail.fr>
2023-02-27 18:20:20 +0100

    add regex on entity menu in urlimage with assert for accepting only image file

--- src/Controller/Admin/MenuCrudController.php
+++ src/Controller/Admin/MenuCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Menu;
+use Symfony\Component\Mime\Part\File;
 use Doctrine\ORM\EntityManagerInterface;
+
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -42,12 +44,13 @@ class MenuCrudController extends AbstractCrudController
             IdField::new('id')->hideOnForm(),
             TextField::new('Title'),
             TextField::new('Price'),
+
+
             ImageField::new('url_image')
                     ->setUploadDir('public/img/menus')
                     ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
                     ->hideOnIndex()
-                    ->setHelp('.jpg or .png only')
-                    ->setFormTypeOptions([]),
+                    ->setHelp('.jpg or .png only'),
             DateTimeField::new('createdAt')
                 ->hideOnForm(),
             AssociationField::new('entree')
--- src/Entity/Menu.php
+++ src/Entity/Menu.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MenuRepository::class)]
 class Menu
@@ -23,6 +24,11 @@ class Menu
     private ?string $Price = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Regex(
+        pattern :' /(\.|\/)(jpe?g|png)$/i' ,
+        match:true,
+        message :'Your file extension is not supported',
+    )]
     private ?string $url_image = null;
 
     #[ORM\Column]
