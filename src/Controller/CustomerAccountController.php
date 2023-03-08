639eccd9d65d743ea09808349c66109df4add2e0
AubourgA <adrien.aubourg@hotmail.fr>
2023-03-08 12:21:26 +0100

    integration account page

--- src/Controller/CustomerAccountController.php
+++ src/Controller/CustomerAccountController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Customer;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CustomerAccountController extends AbstractController
 {
     #[Route('/customer/account', name: 'app_customer_account')]
-    public function index(): Response
+    public function index(BookingRepository $bookingRepository): Response
     {
+
+
+
         return $this->render('customer_account/index.html.twig', [
-            'controller_name' => 'CustomerAccountController',
+           'bookings' => $bookingRepository->findBy(['customer' => $this->getUser()])
         ]);
+    }
+
+    #[Route('/customer/account/modify', name: 'app_customer_modify')]
+    public function modify(Request $request, EntityManagerInterface $em):Response
+    {
+        $customer = $this->getUser();
+
+      
+
+        dd($customer);
+
+        $allergy = $request->get('allergy');
+        $number = $request->get('person');
+
+        dd($allergy);
+        
     }
 }
